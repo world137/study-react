064a96f2e5b432da67e1cff2fa5488c2a870622c
wordl137 <world.sk137@gmail.com>
2022-04-25 18:30:54 +0700

    4.24.20

---
+++ src/component/hello.js
@@ -0,0 +1,4 @@
+function Hello3(){
+    return <h1>hello3</h1>
+}
+export default Hello3;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //library ที่ใช้ในการเรียกการแสดงผล
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Hello3 from './component/hello'
 
 
+// import App from './App';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode> 
+// );
+
+
+class Hello extends React.Component{
+	render(){
+		return<h1>Hello</h1>
+	}
+}
+const root2 = ReactDOM.createRoot(document.getElementById('root2'))
+root2.render(<React.StrictMode><Hello/></React.StrictMode>);
+
+function Hello2(){
+  return <h1>Hello2</h1>
+}
+const root3 = ReactDOM.createRoot(document.getElementById('root3'))
+root3.render(<React.StrictMode><Hello2/></React.StrictMode>)
+
+const root4 = ReactDOM.createRoot(document.getElementById('root4'))
+root4.render(<React.StrictMode><Hello3/></React.StrictMode>)
 
 reportWebVitals();
